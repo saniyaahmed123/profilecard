842cdffb3bac6bb690f69e61f2d07a4710d22a05
Saniya Ahmed <saniya2012ahmed@gmail.com>
2026-02-10 13:16:23 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,56 @@ var cnic = document.getElementById('cnic');
 var card = document.getElementById('card');
 
 function createProfile() {
-    if(fname==="" && lname === ""&& email=== ""&&cnic===""&&phone===""){
-        alert('please fill all fields')
+    if (
+        fname.value === "" ||
+        lname.value === "" ||
+        email.value === "" ||
+        cnic.value === "" ||
+        phone.value === ""
+    ) {
+        Swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "fill all required fields",
+        });
+
+        return;
     }
+
     var fullname = fname.value + " " + lname.value;
 
     card.innerHTML = `
-        <h1>fullname: ${fullname}</h1>
-        <h4>email: ${email.value}</h4>
-        <h4>phone: ${phone.value}</h4>
-        <h4>cnic: ${cnic.value}</h4>
+    <img src="assets/user (2).png">
+        <h4 class= "name">  ${fullname}</h4>
+        <hr>
+        <div class='info'>
+        <h4>✉️ :  ${email.value}</h4>
+        <h4>📞 : ${phone.value}</h4>
+        <h4># CNIC: ${cnic.value}</h4>
+        </div>
     `;
+    card.style.display = "block"
+}
+function submitForm() {
+    if (
+        fname.value === "" ||
+        lname.value === "" ||
+        email.value === "" ||
+        cnic.value === "" ||
+        phone.value === ""
+    ) {
+        Swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "fill all required fields",
+        });
+
+        return;
+    }
+
+    Swal.fire({
+        title: "success",
+        text: "your form had been submitted",
+        icon: "success"
+    });
 }
